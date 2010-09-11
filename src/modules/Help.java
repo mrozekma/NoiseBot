e1c07ebda7b97bf59d5721eaeec7532f21609cd2
Michael Mrozek <sysop073@gmail.com>
2010-09-11 07:42:03 -0400

    Added PM support for .help

--- src/modules/Help.java
+++ src/modules/Help.java
@@ -6,6 +6,7 @@ import panacea.Condition;
 import panacea.MapFunction;
 
 import main.Message;
+import main.NoiseBot;
 import main.NoiseModule;
 
 import static panacea.Panacea.*;
@@ -21,10 +22,15 @@ public class Help extends NoiseModule {
 	public static final String COLOR_COMMAND = BLUE;
 	public static final String COLOR_ARGUMENT = GREEN;
 	
+	private void message(Message message, String text) {
+		this.bot.sendMessage(message.isPM() ? message.getSender() : NoiseBot.CHAN, text);
+	}
+	
 	@Command("\\.help")
+	@PM("\\.help")
 	public void general(Message message) {
-		this.bot.sendMessage("Use ." + COLOR_COMMAND + "help" + NORMAL + " " + COLOR_MODULE + "MODULE" + NORMAL + " to get examples for a specific module:");
-		this.bot.sendMessage("List of modules: " + implode(sorted(map(filter(this.bot.getModules(), new Condition<NoiseModule>() {
+		message(message, "Use ." + COLOR_COMMAND + "help" + NORMAL + " " + COLOR_MODULE + "MODULE" + NORMAL + " to get examples for a specific module:");
+		message(message, "List of modules: " + implode(sorted(map(filter(this.bot.getModules(), new Condition<NoiseModule>() {
 			@Override public boolean satisfies(NoiseModule module) {return !module.isPrivate();}
 		}), new MapFunction<NoiseModule, String>() {
 			@Override public String map(NoiseModule module) {return COLOR_MODULE + module.getFriendlyName() + NORMAL;}
@@ -32,27 +38,28 @@ public class Help extends NoiseModule {
 	}
 	
 	@Command("\\.help (.+)")
+	@PM("\\.help (.+)")
 	public void specific(Message message, String moduleName) {
 		for(NoiseModule module : this.bot.getModules()) {
 			if(module.isPrivate()) {continue;}
 			if(moduleName.equalsIgnoreCase(module.getFriendlyName())) {
-				this.bot.sendMessage(COLOR_MODULE + module.getFriendlyName() + NORMAL + " module -- " + module.getDescription());
-				this.bot.sendMessage("Examples:");
+				message(message, COLOR_MODULE + module.getFriendlyName() + NORMAL + " module -- " + module.getDescription());
+				message(message, "Examples:");
 				String[] examples = module.getExamples();
 				if(examples == null || examples.length == 0) {
-					this.bot.sendMessage("No examples available");
+					message(message, "No examples available");
 				} else {
 					for(String example : examples) {
 						example = example.replaceAll("^\\.([^ ]+) ", "." + COLOR_COMMAND + "$1" + NORMAL + " ");
 						example = example.replaceAll("_([^_]*)_", COLOR_ARGUMENT + "$1" + NORMAL);
-						this.bot.sendMessage(example);
+						message(message, example);
 					}
 				}
 				return;
 			}
 		}
 		
-		this.bot.sendMessage("Unknown module: " + moduleName);
+		message(message, "Unknown module: " + moduleName);
 	}
 	
 	@Override public String getFriendlyName() {return "Help";}
