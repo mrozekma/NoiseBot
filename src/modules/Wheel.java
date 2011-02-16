825e472a308f86d00af4c2718e20f1103da99972
Michael Mrozek <sysop073@gmail.com>
2011-02-16 14:37:36 -0500

    Revert "Let's see about this..."
    This reverts commit 6b1336b8840ebea0b4354224e69dc21145640fe8.
    
    Revert "Let's increase the stakes..."
    
    This reverts commit 8f939925bbb843acf63ac3fcd13850a5ff9a31ff.

--- src/modules/Wheel.java
+++ src/modules/Wheel.java
@@ -26,7 +26,6 @@ import static modules.Slap.slapUser;
  */
 public class Wheel extends NoiseModule implements Serializable {
 	private Map<String, Integer> victims = new HashMap<String, Integer>();
-	private Map<String, Integer> abusers = new HashMap<String, Integer>();
 	
 	@Command("\\.(?:wheel|spin)")
 	public void wheel(Message message) {
@@ -43,20 +42,10 @@ public class Wheel extends NoiseModule implements Serializable {
 			choice = getRandom(users).getNick();
 		} while(choice.equals(this.bot.getNick()));
 		
-		final String sender = message.getSender();
 		this.victims.put(choice, (this.victims.containsKey(choice) ? this.victims.get(choice) : 0) + 1);
-		this.abusers.put(choice, (this.abusers.containsKey(sender) ? this.abusers.get(sender) : 0) + 1);
 		this.save();
 
 		this.bot.sendAction(slapUser(choice));
-
-		for (int i = this.abusers.get(sender); i != 0; i--) {
-			if (getRandomInt(1, 100) == 42) {
-				this.bot.sendMessage("ChanServ", "op #rhnoise");
-				this.bot.kickVictim(sender, "YOU WIN!");
-				break;
-			}
-		}
 	}
 	
 	@Command("\\.wheelstats")
