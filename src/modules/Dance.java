71481228f06ef393c66f1d5dfcc144e6436d2388
Andy Spencer <andy753421@gmail.com>
2011-06-23 04:38:11 +0000

    NoiseBot has got the moves

---
+++ src/modules/Dance.java
@@ -0,0 +1,31 @@
+package modules;
+
+import main.Message;
+import main.NoiseModule;
+
+/**
+ * Dance!
+ *
+ * @author Andy Spencer (and bash.org)
+ *         Created Jun 23, 2011.
+ */
+public class Dance extends NoiseModule {
+	@Command(".*\\b[Dd][Aa][Nn][Cc][Ee].*")
+	public void fascist(Message message) {
+		this.bot.sendMessage("/me dances :D-<");
+		this.bot.sendMessage("/me dances :D|-<");
+		this.bot.sendMessage("/me dances :D/-<");
+	}
+	
+	@Override public String getFriendlyName() {return "Dance";}
+	@Override public String getDescription() {return "Dances when the mood is right";}
+	@Override public String[] getExamples() {
+		return new String[] {
+			"<Zybl0re> get up",
+			"<Zybl0re> get on up",
+			"<Zybl0re> get up",
+			"<Zybl0re> get on up",
+			"<phxl|paper> and DANCE"
+		};
+	}
+}
