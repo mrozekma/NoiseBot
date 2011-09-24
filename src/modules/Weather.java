046560f680e31041f0e07cf62acef2e7a7e86642
Michael Auchter <michael.auchter@gmail.com>
2011-09-24 06:01:59 +0000

    Add Weather module.

---
+++ src/modules/Weather.java
@@ -0,0 +1,77 @@
+package modules;
+
+import static org.jibble.pircbot.Colors.*;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import main.Message;
+import main.NoiseModule;
+
+import static panacea.Panacea.*;
+
+/**
+ * Weather
+ * Based off the SO module
+ *
+ * @author Michael Auchter
+ *         Created Sep 23, 2011.
+ */
+public class Weather extends NoiseModule
+{
+	private static final String WEATHER_URL = "http://weather.yahooapis.com/forecastjson?w=";
+	private static final String COLOR_INFO = PURPLE;
+	private static final String COLOR_ERROR = RED + REVERSE;
+
+	private static final String[][] cities = {
+		{ "12778384", "Terre Haute, IN"},
+		{ "2357536",  "Austin, TX"},
+		{ "2517274",  "West Lafayette, IN"},
+		{ "2401279",  "Fairbanks, AK"},
+		{ "2402488",  "Farmington Hills, MI"},
+		{ "2470874",  "Petaluma, CA"},
+		{ "2490383",  "Seattle, WA"},
+	};
+
+	private static JSONObject getJSON(String url) throws IOException, JSONException {
+		final URLConnection c = new URL(url).openConnection();
+		final DataInputStream s = new DataInputStream(c.getInputStream());
+		final StringBuffer b = new StringBuffer();
+		
+		byte[] buffer = new byte[1024];
+		int size;
+		while((size = s.read(buffer)) >= 0)
+			b.append(new String(buffer, 0, size));
+
+		return new JSONObject(b.toString());
+	}
+
+	public String getWeather(String woe, String location) throws Exception
+	{
+		final JSONObject j = getJSON(WEATHER_URL + woe);
+		final double temperature = j.getJSONObject("condition").getDouble("temperature");
+		final String text  = j.getJSONObject("condition").getString("text");
+
+		return PURPLE + "[" + location + ": " + text + ", " + temperature + "F] ";
+	}
+
+	@Command(".weather")
+	public void weather(Message message)
+	{
+		try {
+			for (int i = 0; i < cities.length; i++)
+				this.bot.sendMessage(getWeather(cities[i][0], cities[i][1]));
+		} catch (Exception e) {
+			this.bot.sendMessage(COLOR_ERROR + "Problem parsing Weather data");
+		}
+	}
+
+	@Override public String getFriendlyName() { return "Weather"; }
+	@Override public String getDescription() { return "Outputs the current weather conditions in cities occupied by #rhnoise"; }
+	@Override public String[] getExamples() { return new String[] { ".weather" }; }
+}
