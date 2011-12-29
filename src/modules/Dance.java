a45b037dd070900e5d709c5f8923dc5d83e0a2c1
Michael Mrozek <git@mrozekma.com>
2011-12-28 22:35:00 -0500

    Let's disco!

--- src/modules/Dance.java
+++ src/modules/Dance.java
@@ -1,5 +1,7 @@
 package modules;
 
+import static org.jibble.pircbot.Colors.*;
+
 import main.Message;
 import main.NoiseModule;
 
@@ -10,11 +12,23 @@ import main.NoiseModule;
  *         Created Jun 23, 2011.
  */
 public class Dance extends NoiseModule {
+	private static final String COLOR_FIRST_GUY = YELLOW;
+	private static final String COLOR_SECOND_GUY = BLUE;
+	private static final String COLOR_FIRST_LINE = RED;
+	private static final String COLOR_SECOND_LINE = GREEN;
+	
 	@Command(".*\\b[Dd][Aa][Nn][Cc][Ee].*")
 	public void dance(Message message) {
 		this.bot.sendAction("dances :D-<");
 		this.bot.sendAction("dances :D|-<");
 		this.bot.sendAction("dances :D/-<");
+	}
+	
+	@Command(".*\\b[Dd][Ii][Ss][Cc][Oo].*")
+	public void disco(Message message) {
+		this.bot.sendMessage(String.format("%s\\o   %sLET'S   %so/", COLOR_FIRST_GUY, COLOR_FIRST_LINE, COLOR_SECOND_GUY));
+		this.bot.sendMessage(String.format(" %s|>  %sDISCO! %s<|", COLOR_FIRST_GUY, COLOR_SECOND_LINE, COLOR_SECOND_GUY));
+		this.bot.sendMessage(String.format("%s< \\         %s/ >", COLOR_FIRST_GUY, COLOR_SECOND_GUY));
 	}
 	
 	@Override public String getFriendlyName() {return "Dance";}
