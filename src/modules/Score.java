2827f558874be7da0334aab3bb79782319a25331
Greg <arathald@arathald-VirtualBox.(none)>
2012-01-10 18:00:32 -0800

    Adding Score command to track users' scores

---
+++ src/modules/Score.java
@@ -0,0 +1,62 @@
+package modules;
+
+import static org.jibble.pircbot.Colors.*;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import main.Message;
+import main.NoiseModule;
+
+
+/**
+ * Score
+ *
+ * @author Arathald (Greg Jackson)
+ *         Created January 10, 2012.
+ */
+
+public class Score extends NoiseModule implements Serializable {
+	private static final String COLOR_POSITIVE = BLACK;
+	private static final String COLOR_NEGATIVE = RED;
+	
+	private Map<String, Integer> userScores = new HashMap<String, Integer>();
+	
+	@Command("(.+)\\+\\+")
+	public void incrementScore(Message message, String target) {
+		this.changeScore(target, 1);
+	}
+	
+	@Command("(.+)\\-\\-")
+	public void decrementScore(Message message, String target) {
+		this.changeScore(target, -1);
+	}
+	
+	@Command("\\.score (.+)")
+	public void getScore(Message message, String target) {
+		if (this.userScores.containsKey(target)) {
+			Integer score = this.userScores.get(target);
+			String color = (score >= 0 ? this.COLOR_POSITIVE : this.COLOR_NEGATIVE);
+			this.bot.sendMessage(target + "'s score is " + color + score);
+		} else {
+			this.bot.sendMessage(target + " has no score");
+		}
+	}
+	
+	@Override public String getFriendlyName() {return "Score";}
+	@Override public String getDescription() {return "Keeps users' scores";}
+	@Override public String[] getExamples() {
+		return new String[] {
+				"_nick_++ -- Increment _nick_'s score",
+				"_nick_-- -- Decrement _nick_'s score",
+				".score _nick_ -- Display _nick_'s score"
+		};
+	}
+	
+	private void changeScore(String user, Integer amount) {
+		Integer oldScore = (this.userScores.containsKey(user) ? this.userScores.get(user) : 0);
+		this.userScores.put(user, oldScore + amount);
+		this.save();
+	}
+}
