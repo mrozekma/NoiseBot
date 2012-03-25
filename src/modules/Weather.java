2738b99567dbf45eb637a689e1cc473d33cb4915
Andy Spencer <andy753421@gmail.com>
2012-03-25 21:41:39 +0000

    Update weather module, add .wx command

--- src/modules/Weather.java
+++ src/modules/Weather.java
@@ -2,6 +2,7 @@ package modules;
 
 import static org.jibble.pircbot.Colors.*;
 
+import java.util.*;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.URL;
@@ -32,14 +33,14 @@ public class Weather extends NoiseModule
 	private static final String COLOR_ERROR = RED + REVERSE;
 
 	private static final String[][] cities = {
-		{ "12778384", "Terre Haute, IN"},
-		{ "2357536",  "Austin, TX"},
-		{ "2374418",  "Canoga Park, CA"},
-		{ "2401279",  "Fairbanks, AK"},
-		{ "2402488",  "Farmington Hills, MI"},
-		{ "2470874",  "Petaluma, CA"},
-		{ "2490383",  "Seattle, WA"},
-		{ "2517274",  "West Lafayette, IN"},
+		{ "12778384", "Terre Haute", "IN"},
+		{ "2357536",  "Austin", "TX"},
+		{ "2374418",  "Canoga Park", "CA"},
+		{ "2401279",  "Fairbanks", "AK"},
+		{ "2402488",  "Farmington Hills", "MI"},
+		{ "2470874",  "Petaluma", "CA"},
+		{ "2490383",  "Seattle", "WA"},
+		{ "2517274",  "West Lafayette", "IN"},
 	};
 
 	private static final String[][] icons = {
@@ -52,6 +53,12 @@ public class Weather extends NoiseModule
 		{"Snow", "\u00e2\u0098\u0083"}, /* SNOWMAN */
 		{"Sunny", "\u00e2\u0098\u0080"}, /* BLACK SUN WITH RAYS */
 		{"Hail", "\u00e2\u0098\u0084"}, /* COMET */
+	};
+
+	private static final String[][] names = {
+		{"Partly Cloudy", "cloudy-"},
+		{"Mostly Cloudy", "cloudy+"},
+		{"Thunderstorms", "storms"},
 	};
 
 	private static JSONObject getJSON(String url) throws IOException, JSONException {
@@ -67,35 +74,63 @@ public class Weather extends NoiseModule
 		return new JSONObject(b.toString());
 	}
 
-	public String getWeather(String woe, String location) throws Exception
+	public List<Map<String,String>> getWeather()
 	{
-		final JSONObject j = getJSON(WEATHER_URL + woe);
-		final double temperature = j.getJSONObject("condition").getDouble("temperature");
-		String text  = j.getJSONObject("condition").getString("text");
+		ArrayList list = new ArrayList();
+		try {
+			for (int i = 0; i < cities.length; i++) {
+				final String uri = WEATHER_URL + cities[i][0];
+				final JSONObject cond = getJSON(uri).getJSONObject("condition");;
 
-		for (int i = 0; i < icons.length; i++)
-			text = text.replace(icons[i][0], icons[i][1]);
+				String icon = cond.getString("text");
+				for (int j = 0; j < icons.length; j++)
+					icon = icon.replace(icons[j][0], icons[j][1]);
+				String txt = cond.getString("text");
+				for (int j = 0; j < names.length; j++)
+					txt = txt.replace(names[j][0], names[j][1]);
 
-		return	COLOR_INFO + "[" +
-			COLOR_LOC  + location +
-			COLOR_INFO + ": " +
-			COLOR_TEXT + text +
-			COLOR_INFO + ", " +
-			COLOR_TEMP + temperature + "F" +
-			COLOR_INFO + "] ";
+				HashMap map = new HashMap();
+				map.put("city",  cities[i][1]);
+				map.put("state", cities[i][2]);
+				map.put("temp",  "" + cond.getInt("temperature"));
+				map.put("text",  cond.getString("text"));
+				map.put("txt",   txt.toLowerCase());
+				map.put("icon",  icon);
+				list.add(map);
+			}
+		} catch (Exception e) {
+			this.bot.sendMessage(COLOR_ERROR + "Problem parsing Weather data");
+		}
+		return list;
 	}
+
 
 	@Command(".weather")
 	public void weather(Message message)
 	{
-		try {
-			String s = "";
-			for (int i = 0; i < cities.length; i++)
-				s += getWeather(cities[i][0], cities[i][1]);
-			this.bot.sendMessage(s);
-		} catch (Exception e) {
-			this.bot.sendMessage(COLOR_ERROR + "Problem parsing Weather data");
-		}
+		List<String> list = new ArrayList();
+		for (Map<String,String> wx : getWeather())
+			list.add(
+				COLOR_INFO + "[" +
+				COLOR_LOC  + wx.get("city") + ", " + wx.get("state") +
+				COLOR_INFO + ": " +
+				COLOR_TEXT + wx.get("text") +
+				COLOR_INFO + ", " +
+				COLOR_TEMP + wx.get("temp") + "F" +
+				COLOR_INFO + "]");
+		this.bot.sendMessage(implode(list.toArray(new String[0]), " "));
+	}
+
+	@Command(".wx")
+	public void wx(Message message)
+	{
+		List<String> list = new ArrayList();
+		for (Map<String,String> cond : getWeather())
+			list.add(
+				cond.get("city") + " " +
+				cond.get("temp") + " " +
+				cond.get("txt"));
+		this.bot.sendMessage(implode(list.toArray(new String[0]), "  |  "));
 	}
 
 	@Override public String getFriendlyName() { return "Weather"; }
