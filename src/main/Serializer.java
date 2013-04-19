8f43ec15d5185054d8b65c7e0983d77774644461
Michael Mrozek <git@mrozekma.com>
2013-04-18 21:49:58 -0400

    Flush the file after serializing

--- src/main/Serializer.java
+++ src/main/Serializer.java
@@ -8,15 +8,23 @@ import java.io.IOException;
 
 import com.google.gson.Gson;
 
+import debugging.Log;
+
 public class Serializer {
 	public static final String ROOT = "store";
 	private static final Gson gson = new Gson();
   
 	public static void serialize(String store, Object data) throws IOException {
-		gson.toJson(data, new FileWriter(new File(ROOT, store)));
+		final File file = new File(ROOT, store);
+		final FileWriter w = new FileWriter(file);
+		Log.d(String.format("Writing %s to %s", data, file.getAbsolutePath()));
+		gson.toJson(data, w);
+		w.close();
 	}
   
 	public static <T> T deserialize(String store, Class<T> type) throws FileNotFoundException {
-		return gson.fromJson(new FileReader(new File(ROOT, store)), type);
+		final File file = new File(ROOT, store);
+		Log.d(String.format("Reading from %s", file.getAbsolutePath()));
+		return gson.fromJson(new FileReader(file), type);
 	}
 }
