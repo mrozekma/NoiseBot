71c9393f2cd9792cd532fe67cc855527533cb0d4
Michael Auchter <michael.auchter@gmail.com>
2014-02-05 05:31:53 +0000

    cleanup of BeerAdvocate, I think.
    Who am I kidding, I don't even have java installed on this machine.

--- src/modules/BeerAdvocate.java
+++ src/modules/BeerAdvocate.java
@@ -20,7 +20,6 @@ import static main.Utilities.*;
  */
 public class BeerAdvocate extends NoiseModule {
   private static final String COLOR_ERROR = RED + REVERSE;
-  private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36";
 
   // Yeah, you wanna fight?
   public static String extract(Document page, String selector)
@@ -31,20 +30,22 @@ public class BeerAdvocate extends NoiseModule {
     return node.text();
   }
 
-  @Command(".*(http://beeradvocate.com/beer/profile/[0-9]+/[0-9]+).*")
-  public void beer(Message message, String beerUrl)
+  private Document snarf(String url)
   {
-    Document page = null;
+    final String USER_AGENT = "Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36";
     try {
-      page = Jsoup.connect(beerUrl)
-                  .timeout(10000)
-                  .userAgent(USER_AGENT)
-                  .get();
+      return Jsoup.connect(url).timeout(10000).userAgent(USER_AGENT).get();
     } catch (IOException e) {
       e.printStackTrace();
       this.bot.sendMessage(COLOR_ERROR + "Error retrieving BA page...");
+      return null;
     }
+  }
 
+  @Command(".*(http://beeradvocate.com/beer/profile/[0-9]+/[0-9]+).*")
+  public void beer(Message message, String beerUrl)
+  {
+    Document page = snarf(beerUrl);
     String name = extract(page, ".titleBar");
     String score = extract(page, ".BAscore_big");
     String style = extract(page, "[href^=/beer/style/]");
@@ -56,28 +57,12 @@ public class BeerAdvocate extends NoiseModule {
   @Command("\\.beer (.*)")
   public void search(Message message, String toSearch)
   {
-    Document searchResults = null;
     toSearch = toSearch.replaceAll(" ", "\\+");
-               toSearch = "\""+toSearch+"\"";
-    try {
-      searchResults = Jsoup.connect("http://beeradvocate.com/search?q="+toSearch+"&qt=beer")
-                                                                       .timeout(10000)
-                                                                       .userAgent(USER_AGENT)
-                                                                       .get();
-                       //this.bot.sendMessage("DEBUG: " + "http://beeradvocate.com"+searchResults.getElementsByTag("li").html());
-      String url = "http://beeradvocate.com"+searchResults
-                                                                       .body()
-                                                                       .getElementsByTag("li")
-                                                                       .get(2)
-                                                                       .getElementsByTag("a")
-                                                                       .get(0)
-                                                                       .attr("href");
-      this.beer(null, url);
-      this.bot.sendMessage(url);
-    } catch(IOException e) {
-      e.printStackTrace();
-      this.bot.sendMessage(COLOR_ERROR + "Error retrieving BA page...");
-    }
+    Document searchResults = snarf("http://beeradvocate.com/search?q=\""+toSearch+"\"&qt=beer");
+    String rel = searchResults.body().getElementsByTag("li").get(2).getElementsByTag("a").get(0).attr("href");
+    String url = "http://beeradvocate.com" + rel;
+    this.beer(null, url);
+    this.bot.sendMessage(url);
   }
 
   @Override
