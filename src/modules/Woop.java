ccef242fe4daa32e22bc133735e47fc2e71380c6
arathald <jacksogc@gmail.com>
2014-03-30 16:42:36 -0700

    added woop coffee

--- src/modules/Woop.java
+++ src/modules/Woop.java
@@ -13,16 +13,13 @@ import static org.jibble.pircbot.Colors.*;
  *         Created Jun 16, 2009. + 1 year, 9 months
  */
 public class Woop extends NoiseModule {
+   private static String REGULAR_WOOP = "WOOP";
+   private static String COFFEE_WOOP = "COFFEE";
+
    @Command("\\.woop ([0-9]+\\.?[0-9]*)")
    public void woop(Message message, String woopsArg) {
 	  float requestedWoops = Float.valueOf(woopsArg);
-	  
-	  int wholeWoops = (int) Math.floor(requestedWoops);
-	  int boundedWoops = range(wholeWoops, 1, 20);
-	  int woopPart = Math.round((requestedWoops - wholeWoops) * 4);
-	  
-	  String woops = new String(new char[boundedWoops]).replace("\0", "WOOP ") + "WOOP".substring(0, woopPart);
-	
+     String woops = generateWoops(REGULAR_WOOP, requestedWoops);
 	  this.bot.sendMessage(RED + woops.trim());
    }
 
@@ -35,6 +32,19 @@ public class Woop extends NoiseModule {
    @Command("\\.woo([o]+)p")
    public void woopLongDefault(Message message, String numOs) {this.woop(message, "" + (numOs.length() + 2));}
 
+   @Command("\\.woop coffee ([0-9]+\\.?[0-9]*)")
+   public void woopCoffee(Message message, String woopsArg) {
+     float requestedWoops = Float.valueOf(woopsArg);
+     String woops = generateWoops(COFFEE_WOOP, requestedWoops);
+     this.bot.sendMessage(RED + woops.trim());
+   }
+
+   @Command(".woop coffee")
+   public void woopCoffeeDefault(Message message) {this.woopCoffee(message, "10");}
+
+   @Command("\\.woo([o]+)p coffee")
+   public void woopCoffeeLongDefault(Message message, String numOs) {this.woopCoffee(message, "" + (numOs.length() + 2));}
+
    @Override public String getFriendlyName() {return "Woop";}
    @Override public String getDescription() {return "Woop it up a bit (to the nearest quarter woop).";}
    @Override public String[] getExamples() {
@@ -42,7 +52,18 @@ public class Woop extends NoiseModule {
             ".woop",
             ".woop 15",
             ".woop 4.25",
-            ".woooooop"
+            ".woooooop",
+            ".woop coffee"
+            ".woop coffee 2.5"
       };
+   }
+
+   private String generateWoops(String woop, float requestedWoops) {
+     int wholeWoops = (int) Math.floor(requestedWoops);
+     int boundedWoops = range(wholeWoops, 1, 20);
+     int woopPart = Math.round((requestedWoops - wholeWoops) * woop.length);
+     
+     String woops = new String(new char[boundedWoops]).replace("\0", woop + " ") + woop.substring(0, woopPart);
+     return woops;
    }
 }
