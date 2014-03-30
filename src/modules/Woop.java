86abf02163678a6e7044e193e3920e36520f0497
arathald <jacksogc@gmail.com>
2014-03-30 16:57:20 -0700

    different approach to woop coffee

--- src/modules/Woop.java
+++ src/modules/Woop.java
@@ -16,47 +16,27 @@ public class Woop extends NoiseModule {
    private static String REGULAR_WOOP = "WOOP";
    private static String COFFEE_WOOP = "COFFEE";
 
-   @Command("\\.woop ([0-9]+\\.?[0-9]*)")
-   public void woop(Message message, String woopsArg) {
+   @Command("\\.woop (coffee)? ?([0-9]+\\.?[0-9]*)")
+   public void woop(Message message, String coffeeArg, String woopsArg) {
 	  float requestedWoops = Float.valueOf(woopsArg);
-     String woops = generateWoops(REGULAR_WOOP, requestedWoops);
+     String woopType = REGULAR_WOOP;
+
+     if (coffeeArg != null && coffeeArg.equals("coffee")) {
+       woopType = COFFEE_WOOP;
+     }
+
+     String woops = generateWoops(woopType, requestedWoops);
 	  this.bot.sendMessage(RED + woops.trim());
    }
 
-   @Command("\\.woop")
-   public void woopDefault(Message message) {this.woop(message, "10");}
+   @Command("\\.woop (coffee)?")
+   public void woopDefault(Message message, String coffeeArg) {this.woop(message, coffeeArg, "10");}
    
-   @Command("\\.woo[o]+p ([0-9]+\\.?[0-9]*)")
-   public void woopLong(Message message, String woopsArg) {this.woop(message, woopsArg);}
+   @Command("\\.woo[o]+p (coffee)? ?([0-9]+\\.?[0-9]*)")
+   public void woopLong(Message message, String, coffeeArg, String woopsArg) {this.woop(message, coffeeArg, woopsArg);}
    
-   @Command("\\.woo([o]+)p")
-   public void woopLongDefault(Message message, String numOs) {this.woop(message, "" + (numOs.length() + 2));}
-
-   @Command("\\.woop coffee ([0-9]+\\.?[0-9]*)")
-   public void woopCoffee(Message message, String woopsArg) {
-     float requestedWoops = Float.valueOf(woopsArg);
-     String woops = generateWoops(COFFEE_WOOP, requestedWoops);
-     this.bot.sendMessage(RED + woops.trim());
-   }
-
-   @Command(".woop coffee")
-   public void woopCoffeeDefault(Message message) {this.woopCoffee(message, "10");}
-
-   @Command("\\.woo([o]+)p coffee")
-   public void woopCoffeeLongDefault(Message message, String numOs) {this.woopCoffee(message, "" + (numOs.length() + 2));}
-
-   @Override public String getFriendlyName() {return "Woop";}
-   @Override public String getDescription() {return "Woop it up a bit (to the nearest quarter woop).";}
-   @Override public String[] getExamples() {
-      return new String[] {
-            ".woop",
-            ".woop 15",
-            ".woop 4.25",
-            ".woooooop",
-            ".woop coffee"
-            ".woop coffee 2.5"
-      };
-   }
+   @Command("\\.woo([o]+)p (coffee)?")
+   public void woopLongDefault(Message message, String numOs, String coffeeArg) {this.woop(message, coffeeArg, "" + (numOs.length() + 2));}
 
    private String generateWoops(String woop, float requestedWoops) {
      int wholeWoops = (int) Math.floor(requestedWoops);
