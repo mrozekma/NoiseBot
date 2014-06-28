a016b98a7c01cba7ba6d2b981538ef5cca792c0f
Michael Mrozek <git@mrozekma.com>
2014-06-28 03:22:52 -0400

    Added submission time to Youtube

--- src/modules/Youtube.java
+++ src/modules/Youtube.java
@@ -7,6 +7,12 @@ import java.io.UnsupportedEncodingException;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -31,6 +37,8 @@ import static main.Utilities.formatSeconds;
  *         Created Jun 17, 2009.
  */
 public class Youtube extends NoiseModule {
+	private static final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX");
+
 	private static final String COLOR_ERROR = RED;
 	private static final String COLOR_INFO = PURPLE;
 
@@ -43,15 +51,17 @@ public class Youtube extends NoiseModule {
 
 			// There should be exactly one entry for every video ID
 			if(entryList.getLength() == 0) {
-				this.bot.sendMessage(COLOR_ERROR + "Unable to find Youtube video with ID " + videoID);
+				throw new FileNotFoundException(); // Youtube's HTTP code should cause this automatically, this case should never happen
 			} else if(entryList.getLength() != 1) {
 				this.bot.sendMessage(COLOR_ERROR + "Found " + entryList.getLength() + " videos with ID " + videoID);
+				return;
 			}
 
 			final Node entry = entryList.item(0);
 
 			String author = null, title = null;
 			int duration = 0, viewCount = 0;
+			Calendar published = null;
 
 			final NodeList entryRootNodes = entry.getChildNodes();
 			for(int i = 0; i < entryRootNodes.getLength(); i++) {
@@ -62,7 +72,6 @@ public class Youtube extends NoiseModule {
 						final Node authorNode = authorNodes.item(j);
 						if(authorNode.getNodeName().equals("name")) {
 							author = authorNode.getTextContent();
-							continue;
 						}
 					}
 				} else if(entryRootNode.getNodeName().equals("media:group")) {
@@ -71,20 +80,26 @@ public class Youtube extends NoiseModule {
 						final Node mediaNode = mediaNodes.item(j);
 						if(mediaNode.getNodeName().equals("media:title")) {
 							title = mediaNode.getTextContent();
-							continue;
 						} else if(mediaNode.getNodeName().equals("yt:duration")) {
 							duration = Integer.parseInt(mediaNode.getAttributes().getNamedItem("seconds").getNodeValue());
-							continue;
 						}
 					}
 				} else if(entryRootNode.getNodeName().equals("yt:statistics")) {
 					viewCount = Integer.parseInt(entryRootNode.getAttributes().getNamedItem("viewCount").getNodeValue());
-					continue;
+				} else if(entryRootNode.getNodeName().equals("published")) {
+					published = new GregorianCalendar();
+					try {
+						published.setTime(dateFormat.parse(entryRootNode.getTextContent()));
+					} catch(ParseException e) {
+						Log.e(e);
+						published = null;
+					}
 				}
 			}
 
-			if(author != null  && title != null) {
-				this.bot.sendMessage(COLOR_INFO +  encoded(title) + " (posted by " + encoded(author) + ", " + formatSeconds(duration) + ", " + viewCount + " views)");
+			if(author != null && title != null) {
+				final String pubdate = (published == null) ? null : String.format("%d %s %d", published.get(Calendar.DAY_OF_MONTH), published.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.getDefault()), published.get(Calendar.YEAR));
+				this.bot.sendMessage(COLOR_INFO +  encoded(title) + " (posted by " + encoded(author) + (pubdate == null ? "" : " on " + pubdate) + ", " + formatSeconds(duration) + ", " + viewCount + " views)");
 			} else {
 				this.bot.sendMessage(COLOR_ERROR + "Problem parsing Youtube data");
 			}
@@ -92,7 +107,7 @@ public class Youtube extends NoiseModule {
 			this.bot.sendMessage(COLOR_ERROR + "Unable to parse Youtube data");
 			Log.e(e);
 		} catch(FileNotFoundException e) {
-			this.bot.sendMessage(COLOR_ERROR + "Video not found");
+			this.bot.sendMessage(COLOR_ERROR + "Unable to find Youtube video with ID " + videoID);
 			Log.e(e);
 		} catch(MalformedURLException e) {
 			this.bot.sendMessage(COLOR_ERROR + "Unable to contact Youtube");
