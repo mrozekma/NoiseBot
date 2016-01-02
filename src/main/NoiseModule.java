fa0d136947b0b5ab370889afacbab89a10f88d68
Michael Mrozek <git@mrozekma.com>
2016-01-01 23:38:38 -0500

    Add View infrastructure; no modules use it yet. As a side-effect, fixed a long-standing caching problem that would occasionally mess with reloaded modules (the singleton javassist ClassPool would keep references to old versions of the module lying around)

--- src/main/NoiseModule.java
+++ src/main/NoiseModule.java
@@ -2,28 +2,14 @@ package main;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.UnsupportedEncodingException;
 import java.io.Serializable;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import java.util.TreeSet;
 
 import javassist.ClassPool;
 import javassist.CtClass;
@@ -37,6 +23,7 @@ import org.jibble.pircbot.User;
 import static org.jibble.pircbot.Colors.*;
 
 import debugging.Log;
+import org.json.JSONObject;
 
 
 /**
@@ -50,22 +37,29 @@ public abstract class NoiseModule implements Comparable<NoiseModule> {
 
 	@Retention(RetentionPolicy.RUNTIME)
 	@Target(ElementType.METHOD)
-	protected static @interface Command {
+	protected @interface Command {
 		String value(); // Regex pattern (must be named 'value' for Java reasons)
 		boolean caseSensitive() default true; // Pattern is case-sensitive
 	}
 
 	@Retention(RetentionPolicy.RUNTIME)
 	@Target(ElementType.METHOD)
-	protected static @interface PM {
+	protected @interface PM {
 		String value();
 		boolean caseSensitive() default true;
+	}
+
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target(ElementType.METHOD)
+	protected @interface View {
+		Protocol[] value() default {}; // protocol(s) this view is used for. Empty list if this is the default if no more-specific view exists
+		String method() default ""; // Apparently null isn't a constant, so I can't use it for the default value
 	}
 
 	// Field is loaded from the config file
 	@Retention(RetentionPolicy.RUNTIME)
 	@Target(ElementType.FIELD)
-	protected static @interface Configurable {
+	protected @interface Configurable {
 		String value(); // Config key
 		boolean required() default true; // If the field is null, this module won't fire
 	}
@@ -83,47 +77,24 @@ public abstract class NoiseModule implements Comparable<NoiseModule> {
 		}};
 		final StrSubstitutor sub = new StrSubstitutor(variables);
 
-		final TreeSet<CtMethod> methods = new TreeSet<CtMethod>(new Comparator<CtMethod>() {
-			@Override public int compare(CtMethod first, CtMethod second) {
-				return Integer.compare(first.getMethodInfo().getLineNumber(0), second.getMethodInfo().getLineNumber(0));
-			}
-		});
 		try {
-			for(CtMethod ctmethod : ClassPool.getDefault().getCtClass(this.getClass().getName()).getDeclaredMethods()) {
-				if(ctmethod.hasAnnotation(Command.class) || ctmethod.hasAnnotation(PM.class)) {
-					methods.add(ctmethod);
+			for(Method method : getAnnotatedMethods(this.getClass(), Command.class, PM.class)) {
+				final Command command = method.getAnnotation(Command.class);
+				if(command != null) {
+					final Pattern pattern = Pattern.compile(sub.replace(command.value()), command.caseSensitive() ? 0 : Pattern.CASE_INSENSITIVE);
+					Log.i(this + " - Added pattern %s for method %s", command.value(), method);
+					this.patterns.put(pattern, method);
+				}
+
+				final PM pm = method.getAnnotation(PM.class);
+				if(pm != null) {
+					final Pattern pattern = Pattern.compile(sub.replace(pm.value()), pm.caseSensitive() ? 0 : Pattern.CASE_INSENSITIVE);
+					Log.i(this + " - Added PM pattern %s for method %s", pm.value(), method);
+					this.pmPatterns.put(pattern, method);
 				}
 			}
-		} catch(NotFoundException e) {
+		} catch(NotFoundException | ClassNotFoundException | NoSuchMethodException e) {
 			throw new ModuleInitException(e);
-		}
-		for(CtMethod ctmethod : methods) {
-			// This is way more difficult than it should be
-			final Method method;
-			try {
-				final CtClass[] ctparams = ctmethod.getParameterTypes();
-				final Class[] params = new Class[ctparams.length];
-				for(int i = 0; i < ctparams.length; i++) {
-					params[i] = ClassUtils.getClass(ctparams[i].getName());
-				}
-				method = this.getClass().getMethod(ctmethod.getName(), params);
-			} catch (NotFoundException | ClassNotFoundException | NoSuchMethodException e) {
-				throw new ModuleInitException(e);
-			}
-
-			final Command command = method.getAnnotation(Command.class);
-			if(command != null) {
-				final Pattern pattern = Pattern.compile(sub.replace(command.value()), command.caseSensitive() ? 0 : Pattern.CASE_INSENSITIVE);
-				Log.i(this + " - Added pattern %s for method %s", command.value(), method);
-				this.patterns.put(pattern, method);
-			}
-
-			final PM pm = method.getAnnotation(PM.class);
-			if(pm != null) {
-				final Pattern pattern = Pattern.compile(sub.replace(pm.value()), pm.caseSensitive() ? 0 : Pattern.CASE_INSENSITIVE);
-				Log.i(this + " - Added PM pattern %s for method %s", pm.value(), method);
-				this.pmPatterns.put(pattern, method);
-			}
 		}
 	}
 
@@ -258,10 +229,57 @@ public abstract class NoiseModule implements Comparable<NoiseModule> {
 							args[i] = matcher.group(i);
 						}
 					}
+					if(!method.getReturnType().equals(Void.TYPE) && !method.getReturnType().equals(JSONObject.class)) {
+						throw new ArgumentMismatchException(String.format("Method returns %s (should either be void or return a JSONObject)", method.getReturnType().getName()));
+					}
 
 					try {
 						Log.v("Invoking with %d args", args.length);
-						method.invoke(this, args);
+						final Object ret = method.invoke(this, args);
+
+						// If ret is null, the method is void. This is the simple case, where the method sends through the bot directly and returns no data
+						// If ret is non-null, it's the JSONObject of data the method returned. We search for a view to display it
+						if(ret != null) {
+							final JSONObject data = (JSONObject)ret;
+							final Class cls = method.getDeclaringClass();
+							final Method[] views = getAnnotatedMethods(cls, View.class);
+
+							// We do this CSS style and look for the most specific view (ties are broken by file-order, which is the order 'views' is in)
+							Method methodAndProtocolMatch = null, methodMatches = null, protocolMatches = null, wildMatch = null;
+							for(Method m : views) {
+								final View view = m.getAnnotation(View.class);
+								final boolean flagMethodMatches = view.method().equals(method.getName());
+								final boolean flagProtocolMatches = Arrays.stream(view.value()).anyMatch(this.bot.getProtocol()::equals);
+								final boolean flagMethodWild = view.method().isEmpty();
+								final boolean flagProtocolWild = view.value().length == 0;
+
+								if(flagMethodMatches && flagProtocolMatches) {
+									if(methodAndProtocolMatch == null) {
+										methodAndProtocolMatch = m;
+									}
+								} else if(flagMethodMatches && flagProtocolWild) {
+									if(methodMatches == null) {
+										methodMatches = m;
+									}
+								} else if(flagProtocolMatches && flagMethodWild) {
+									if(protocolMatches == null) {
+										protocolMatches = m;
+									}
+								} else if(flagMethodWild && flagProtocolWild) {
+									if(wildMatch == null) {
+										wildMatch = m;
+									}
+								}
+							}
+
+							// Choose the best match, or use the default view if there are none
+							final Optional<Method> view = Arrays.asList(new Method[] {methodAndProtocolMatch, methodMatches, protocolMatches, wildMatch}).stream().filter(m -> m != null).findFirst();
+							if(view.isPresent()) {
+								view.get().invoke(this, data);
+							} else {
+								this.defaultView(data);
+							}
+						}
 						break;
 					} catch(Exception e) {
 						Log.e(e);
@@ -271,6 +289,11 @@ public abstract class NoiseModule implements Comparable<NoiseModule> {
 				}
 			}
 		}
+	}
+
+	@View // This method is called directly by processMessage(); the annotation is just for consistency
+	private void defaultView(JSONObject data) {
+		this.bot.sendMessage("" + data);
 	}
 
 	public static <T extends NoiseModule> T load(NoiseBot bot, Class<T> moduleType) {
@@ -318,5 +341,33 @@ public abstract class NoiseModule implements Comparable<NoiseModule> {
 				}
 			}
 		});
+	}
+
+	private static Method[] getAnnotatedMethods(Class cls, Class... annotations) throws NotFoundException, ClassNotFoundException, NoSuchMethodException {
+		// Get methods annotated with at least one of 'annotations', in file order
+		final TreeSet<CtMethod> methods = new TreeSet<CtMethod>((first, second) -> Integer.compare(first.getMethodInfo().getLineNumber(0), second.getMethodInfo().getLineNumber(0)));
+		final ClassPool cp = new ClassPool(); // We do this instead of ClassPool.getDefault() to avoid caching problems with reloaded modules
+		cp.appendSystemPath();
+		for(CtMethod ctmethod : cp.getCtClass(cls.getName()).getDeclaredMethods()) {
+			for(Class annot : annotations) {
+				if(ctmethod.hasAnnotation(annot)) {
+					methods.add(ctmethod);
+					break;
+				}
+			}
+		}
+
+		// Convert javassist types to standard reflection types. This is way more difficult than it should be
+		final List<Method> rtn = new LinkedList<>();
+		for(CtMethod ctmethod : methods) {
+			final CtClass[] ctparams = ctmethod.getParameterTypes();
+			final Class[] params = new Class[ctparams.length];
+			for(int i = 0; i < ctparams.length; i++) {
+				params[i] = ClassUtils.getClass(ctparams[i].getName());
+			}
+			rtn.add(cls.getMethod(ctmethod.getName(), params));
+		}
+
+		return rtn.toArray(new Method[0]);
 	}
 }
