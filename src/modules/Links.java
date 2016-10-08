1db518bedbc345e37a5902a7bf9a67f1842313a1
Michael Mrozek <git@mrozekma.com>
2016-10-08 19:44:45 -0400

    Links has been broken since the switch to views in January... anyway, fixed it. Fixes #62

--- src/modules/Links.java
+++ src/modules/Links.java
@@ -1,11 +1,9 @@
 package modules;
 
 import java.io.Serializable;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
+import debugging.Log;
 import main.*;
 import org.json.JSONException;
 
@@ -24,14 +22,38 @@ public class Links extends NoiseModule implements Serializable {
 		
 		private CachedMessage() {}
 		public CachedMessage(Message message) {
-			this.date = new Date();
+			this(new Date(), message);
+		}
+
+		private CachedMessage(Date date, Message message) {
+			this.date = date;
 			this.message = message;
 		}
 
-		public boolean fromUser(final String user) { return user.equals(this.message.getSender()); }
+		public boolean fromUser(final String user) {
+			return user.equals(this.message.getSender());
+		}
 
-		@Override public String toString() {return this.date + " " + this.message;}
-		@Override public int compareTo(CachedMessage c) { return c.date.compareTo(this.date); }
+		public JSONObject pack() throws JSONException {
+			return new JSONObject()
+					.put("date", this.date)
+					.put("message", this.message.getMessage())
+					.put("sender", this.message.getSender())
+					.put("response_target", this.message.getResponseTarget());
+		}
+
+		public static CachedMessage unpack(JSONObject json) throws JSONException {
+			// This passes null for the 'bot' field in Message, but that field is transient and isn't serialized to the store anyway
+			return new CachedMessage((Date)json.get("date"), new Message(null, json.getString("message"), json.getString("sender"), json.optString("response_target", null)));
+		}
+
+		@Override public String toString() {
+			return this.date + " " + this.message;
+		}
+
+		@Override public int compareTo(CachedMessage c) {
+			return c.date.compareTo(this.date);
+		}
 	}
 
 	private Map<String, CachedMessage> links = new HashMap<>();
@@ -62,13 +84,27 @@ public class Links extends NoiseModule implements Serializable {
 	@Command("\\.(?:lasturls|links) ([0-9]+)")
 	public JSONObject lastURLs(CommandContext ctx, int num) throws JSONException {
 		num = range(num, 1, this.lastN.length);
-		return new JSONObject().put("links", Arrays.stream(this.lastN).filter(url -> url != null).limit(num).map(url -> this.links.get(url)).toArray(CachedMessage[]::new));
+		return new JSONObject().put("links", Arrays.stream(this.lastN).filter(url -> url != null).limit(num).map(url -> {
+			try {
+				return this.links.get(url).pack();
+			} catch(JSONException e) {
+				Log.e(e);
+				return null;
+			}
+		}).filter(json -> json != null).toArray(JSONObject[]::new));
 	}
 
 	@Command("\\.(?:lasturls|links) (\\w+)")
 	public JSONObject lastURLs(CommandContext ctx, String user) throws JSONException {
 		final int num = 5;
-		return new JSONObject().put("links", this.links.values().stream().filter(c -> c.fromUser(user)).sorted().limit(num).toArray(CachedMessage[]::new));
+		return new JSONObject().put("links", this.links.values().stream().filter(c -> c.fromUser(user)).sorted().limit(num).map(c -> {
+			try {
+				return c.pack();
+			} catch(JSONException e) {
+				Log.e(e);
+				return null;
+			}
+		}).filter(json -> json != null).toArray(JSONObject[]::new));
 	}
 
 	@Command("\\.(?:lasturls|links)")
@@ -79,14 +115,27 @@ public class Links extends NoiseModule implements Serializable {
 	@View
 	public void viewLastURLs(ViewContext ctx, JSONObject data) throws JSONException {
 		final JSONArray cms = data.getJSONArray("links");
-		cms.stream().forEach(link -> ctx.respond("#recap %s", link));
+		for(int i = 0; i < cms.length(); i++) {
+			final JSONObject obj = cms.getJSONObject(i);
+			final CachedMessage cm = CachedMessage.unpack(obj);
+			ctx.respond("#recap %s", cm);
+		}
 	}
 
 	@View(Protocol.Slack)
 	public void slackViewLastURLs(ViewContext ctx, JSONObject data) throws JSONException {
-		final CachedMessage[] cms = data.getJavaArray("links", CachedMessage.class);
+		final JSONArray jsonArr = data.getJSONArray("links");
+		final List<CachedMessage> cms = new LinkedList<>();
+		jsonArr.<JSONObject>stream().forEach(json -> {
+			try {
+				cms.add(CachedMessage.unpack(json));
+			} catch(JSONException e) {
+				Log.e(e);
+				ctx.respond("#error %s", e.getMessage());
+			}
+		});
 		//TODO Links come out with <> around them for some reason
-		ctx.respond("#([\n] #recap " + MessageBuilder.BULLET + " %s)", (Object)cms);
+		ctx.respond("#([\n] #recap " + MessageBuilder.BULLET + " %s)", (Object)cms.toArray(new CachedMessage[0]));
 	}
 
 	@Override public String getFriendlyName() {return "Links";}
